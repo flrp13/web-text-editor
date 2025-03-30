fc661a613e6c0ed6710362e62607eb62b432653a
Florin Popa <flrpopa13@gmail.com>
2025-03-30 22:06:36 +0300

    prepare for file browser

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import {settings} from "./settings.js";
+import { settings } from "./settings.js";
 
 const SCALE_FACTOR = 1.4
-const LINE_START_X = settings.fontSize * SCALE_FACTOR
+const NUMBERED_LINE_START = 200
+const LINE_START_X = NUMBERED_LINE_START + settings.fontSize * SCALE_FACTOR
 
 const canvas = document.getElementById('editor')
 const ctx = canvas.getContext("2d");
 const body = document.body
+canvas.style.cursor = 'text'
 
 let textStrRepr = '\tHello world\n I\'m Mike'
 let text // editor text lines
@@ -25,6 +27,10 @@ const initialClickPos = [0, 0]
 const highlightParams = [0, 0, 0, 0] // x, y, width, height
 let isHighlight = false
 
+const file = new Path2D('M213.65723,66.34326l-40-40A8.00076,8.00076,0,0,0,168,24H88A16.01833,16.01833,0,0,0,72,40V56H56A16.01833,16.01833,0,0,0,40,72V216a16.01833,16.01833,0,0,0,16,16H168a16.01833,16.01833,0,0,0,16-16V200h16a16.01833,16.01833,0,0,0,16-16V72A8.00035,8.00035,0,0,0,213.65723,66.34326ZM136,192H88a8,8,0,0,1,0-16h48a8,8,0,0,1,0,16Zm0-32H88a8,8,0,0,1,0-16h48a8,8,0,0,1,0,16Zm64,24H184V104a8.00035,8.00035,0,0,0-2.34277-5.65674l-40-40A8.00076,8.00076,0,0,0,136,56H88V40h76.68652L200,75.314Z')
+let separator
+let isSeparatorHovered = false
+
 
 function init() {
     canvas.height = body.offsetHeight
@@ -35,6 +41,7 @@ function init() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
 
     // Canvas
     ctx.fillStyle = settings.editorBgColor
@@ -45,7 +52,7 @@ function draw() {
     ctx.font = `${settings.fontSize}px Space Mono`
     ctx.fillStyle = 'grey'
     for (let i = 0; i < text.length; i++) {
-        ctx.fillText(i + 1, 20, settings.fontSize * (i + 1))
+        ctx.fillText(i + 1, NUMBERED_LINE_START, settings.fontSize * (i + 1))
     }
 
     if (isHighlight) {
@@ -70,6 +77,19 @@ function draw() {
         ctx.fillRect(...cursorPos, 5, settings.fontSize)
         ctx.fillStyle = settings.fontColor
     }
+
+    ctx.scale(0.15, 0.15);
+    ctx.fillStyle = 'white'
+    ctx.fill(file)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+
+
+    separator = new Path2D()
+    separator.rect(NUMBERED_LINE_START, 0, 3, canvas.height)
+    ctx.fillStyle = isSeparatorHovered ? 'blue' : 'grey'
+    ctx.fill(separator)
+
+
 
     window.requestAnimationFrame(draw)
 }
@@ -282,7 +302,7 @@ function highlightSelection() {
 let leftClickPressed = false
 
 window.addEventListener('mousemove', (e) => {
-    let {clientX: x, clientY: y} = e;
+    let { clientX: x, clientY: y } = e;
 
     if (leftClickPressed && initialClickPos[0] != x) {
 
@@ -294,10 +314,17 @@ window.addEventListener('mousemove', (e) => {
         highlightSelection()
     }
 
+    if (ctx.isPointInPath(separator, x, y)) {
+        isSeparatorHovered = true
+        canvas.style.cursor = 'ew-resize'
+    } else {
+        isSeparatorHovered = false
+        canvas.style.cursor = 'text'
+    }
 })
 
 window.addEventListener('mousedown', (e) => {
-    let {clientX: x, clientY: y} = e;
+    let { clientX: x, clientY: y } = e;
     leftClickPressed = e.button == 0
 
     // handle the case where text is highlighted
@@ -318,7 +345,7 @@ window.addEventListener('mousedown', (e) => {
 })
 
 window.addEventListener('mouseup', (e) => {
-    let {clientX: x, clientY: y} = e;
+    let { clientX: x, clientY: y } = e;
 
     if (leftClickPressed) {
         leftClickPressed = false
